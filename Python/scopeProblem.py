783628f09737ab70d9896139d7eab1440d894b42
hone1er <josephmvillavicencio@gmail.com>
2019-04-13 10:05:24 -0700

    scope problem hackerrank

---
+++ Python/scopeProblem.py
@@ -0,0 +1,23 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+    def __init__(self,elements):
+        self.__elements = elements
+        self.maximumDifference = 0
+	# Add your code here
+    def computeDifference(self):
+        for e in self.__elements:
+            for d in self.__elements:
+                diff = abs(e-d)
+                if diff > self.maximumDifference:
+                    self.maximumDifference = diff
+                
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
