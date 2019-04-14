97df7d8f2f5a39bf00cdc5c9bbba1efaa6f8473b
hone1er <josephmvillavicencio@gmail.com>
2019-04-14 11:43:29 -0700

    apple orange problem

---
+++ Python/appleOrange.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    count = [0,0]
+    for apple in apples:
+        if apple + a in range(s,t+1):
+            count[0] += 1
+    for orange in oranges:
+        if orange + b in range(s,t+1):
+            count[1] += 1
+    for i in count:
+        print(i)
+
+
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
