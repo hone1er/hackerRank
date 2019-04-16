aaecbbca7f06a4e37a63d72583d0b8d6f426db5f
hone1er <josephmvillavicencio@gmail.com>
2019-04-16 09:49:10 -0700

    sql

--- sql/weather5.sql
+++ sql/weather5.sql
@@ -7,4 +7,5 @@ select CITY, length(CITY) from station order by length(CITY) desc, city limit 1;
 /*
 Enter your query here.
 */
-SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*";
+SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*"; /* Cities that start with "aeiou" */
+SELECT DISTINCT city FROM station WHERE city REGEXP ".[aeiou]$";  /* Cities that end with "aeiou" */
