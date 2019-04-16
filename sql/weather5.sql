afed9376d7ab71b0ef8bacf5081d3f1e0773b92f
hone1er <josephmvillavicencio@gmail.com>
2019-04-16 09:59:04 -0700

    sql

--- sql/weather5.sql
+++ sql/weather5.sql
@@ -10,3 +10,4 @@ Enter your query here.
 SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*"; /* Cities that start with "aeiou" */
 SELECT DISTINCT city FROM station WHERE city REGEXP ".[aeiou]$";  /* Cities that end with "aeiou" */
 SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*[aeiou]$" /* Cities that start and end with "aeiou" */
+SELECT DISTINCT city FROM station WHERE city REGEXP "^[^aeiou].*"/* Cities that don't start with "aeiou" */
