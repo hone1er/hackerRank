5e1c5ae5ec3a5c340f18746c80c6f81c42f4b3a9
hone1er <josephmvillavicencio@gmail.com>
2019-04-15 18:15:39 -0700

    weather5.sql

---
+++ sql/weather5.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+*/
+select CITY, length(CITY) from station order by length(CITY), city limit 1;
+select CITY, length(CITY) from station order by length(CITY) desc, city limit 1
